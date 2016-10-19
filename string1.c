97da28aeeb7a482f952518cbdef58d0bd181de59
kishori9 <kasatkishori99@gmail.com>
2016-10-19 13:40:50 +0530

    Update string1.c

--- string1.c
+++ string1.c
@@ -1,26 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"string1.h"
-int strlength(char *str);
-void strcopy(char *str1,char *str2);
-void strconcat(char *str1,char *str2);
-int strcomp( char *str1, char *str2);
-int strncomp(const char *str1, const char *str2, size_t n);
-char *strncopy(char *str1,char *str2, size_t n);
-char *strnconcat(char *str1 , char *str2, size_t n);
-char *stpcopy(char *str1, const char *str2);
-char *strchar(char *str, int c);
-int stringcoll (char *str1, char *str2);
-size_t strcspn(const char *str1, const char *str2);
-char *strrchar(const char *str, int c);
-char *strdupl(char *str);
-void *memcopy(void *dest, const void *src, size_t n);
-void *memmov(void *dest, const void *src, size_t n);
-char *strpbrk(const char *s, const char *accept);
- char *strsep(char **stringp, const char *delim);
-char *strstr1(char *str1, char *str2);
-char *strtok(char *s, const char *delim);
-size_t strxfrm(char *dest, const char *src, size_t n);
 
 int main()
 {
